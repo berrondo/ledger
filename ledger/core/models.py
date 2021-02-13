5eff95e11a04466b8eb0b5f9767b24f92b64d96b
berrondo <claudio.berrondo@gmail.com>
2021-02-13 14:44:38 -0300

    going a little further...

--- ledger/core/models.py
+++ ledger/core/models.py
@@ -3,14 +3,21 @@ from django.db import models
 from polymorphic.models import PolymorphicModel
 
 
-class Transacao(PolymorphicModel):
-    valor = models.DecimalField(decimal_places=2, max_digits=10)
-    created_at = models.DateTimeField(auto_now=True)
-
-
-class Venda(Transacao):
+class Account(models.Model):
     ...
 
 
-class Imposto(Transacao):
+class Transaction(PolymorphicModel):
+    created_at = models.DateTimeField(auto_now=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    debit_from = models.OneToOneField(Account, on_delete=models.PROTECT)
+    credit_to = models.OneToOneField(Account, on_delete=models.PROTECT)
+    # uuid =
+
+
+class Venda(Transaction):
+    ...
+
+
+class Imposto(Transaction):
     ...

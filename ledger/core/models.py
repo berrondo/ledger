5c3bea8a5568b5291be98f0d37190a736bc6f6d3
berrondo <claudio.berrondo@gmail.com>
2021-02-14 18:59:56 -0300

    more simplifying

--- ledger/core/models.py
+++ ledger/core/models.py
@@ -4,26 +4,41 @@ from django.db import models
 from django.db.models import Sum
 from django.utils.datetime_safe import datetime
 
-from polymorphic.models import PolymorphicModel
+
+class Percentual:
+    def __init__(self, percentual):
+        self.percentual = percentual / 100
+
+    def __call__(self, amount):
+        return self.percentual * amount
 
 
 class Account(models.Model):
+    def all_credits(self):
+        return Transaction.objects.filter(
+            credit_to=self
+        ).aggregate(Sum('amount'))['amount__sum'] or Decimal('0.00')
+
+    def all_debits(self):
+        return Transaction.objects.filter(
+            debit_from=self
+        ).aggregate(Sum('amount'))['amount__sum'] or Decimal('0.00')
+
     def balance(self):
-        return Transaction.objects.filter(credit_to=self).aggregate(Sum('amount'))['amount__sum']
+        return self.all_credits() - self.all_debits()
 
 
-class Transaction(PolymorphicModel):
+class Transaction(models.Model):
     created_at = models.DateTimeField()
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     debit_from = models.ForeignKey(Account, on_delete=models.PROTECT)
     credit_to = models.ForeignKey(Account, on_delete=models.PROTECT)
-    # uuid =
 
 
 class Journal:
     def __init__(self, *transactions):
         self.transactions = transactions
-        self.base = list(self.transactions).pop(0)
+        self.base = list(transactions).pop(0)
         self.sub_transactions = transactions
 
     def __call__(self, amount):
@@ -33,8 +48,10 @@ class Journal:
 
     def _process_sub_transactions(self):
         for transaction in self.sub_transactions:
-            transaction.debit_from = self.base.debit_from
-            transaction.credit_to = self.base.credit_to
+            if not hasattr(transaction, 'debit_from'):
+                transaction.debit_from = self.base.debit_from
+            if not hasattr(transaction, 'credit_to'):
+                transaction.credit_to = self.base.credit_to
 
     def save(self):
         created_at = datetime.now()
--- ledger/core/tests/test_transaction.py
+++ ledger/core/tests/test_transaction.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 import pytest
 
-from ledger.core.models import Journal, Transaction, Account
+from ledger.core.models import Journal, Transaction, Account, Percentual
 
 
 @pytest.fixture
@@ -12,6 +12,11 @@ def conta1():
 
 @pytest.fixture
 def conta2():
+    return Account.objects.create()
+
+
+@pytest.fixture
+def conta3():
     return Account.objects.create()
 
 
@@ -55,6 +60,34 @@ def test_venda_com_desconto(conta1, conta2):
     assert all_transactions[1].amount == Decimal(-10)
 
     assert conta2.balance() == Decimal(90)
+
+
+def test_venda_com_imposto(conta1, conta2, conta3):
+    # defining Venda contract:
+    VendaCom10PorcentoDeImposto = Journal(
+        Transaction(
+            debit_from=conta1,
+            credit_to=conta2
+        ),
+        Transaction(
+            amount=-10,
+            credit_to=conta3
+        )
+    )
+
+    # using Venda contract:
+    v = VendaCom10PorcentoDeImposto(100)
+    v.save()
+
+    all_transactions = Transaction.objects.all()
+    assert all_transactions.count() == 2
+    assert all_transactions[0].amount == Decimal(100)
+    assert all_transactions[1].amount == Decimal(-10)
+
+    assert all_transactions[1].credit_to == conta3
+
+    assert conta2.balance() == Decimal(90)
+    assert conta3.balance() == Decimal(10)
 
 
 # some proposals for contract definition:

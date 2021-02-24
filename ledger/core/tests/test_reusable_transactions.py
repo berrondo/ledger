841a7b044693ffc3b59af92980f750fe54b5af29
berrondo <claudio.berrondo@gmail.com>
2021-02-24 03:28:29 -0300

    Transaction is now composable and reusable!
    composability seems to do wrong calculations...

--- ledger/core/tests/test_reusable_transactions.py
+++ ledger/core/tests/test_reusable_transactions.py
@@ -56,21 +56,21 @@ def test_venda_com_imposto_com_comissao_com_imposto_com_composicao_de_transacoes
         from_=cash_in,
         to_=cash_out)
 
-    T('ComissaoPercentual',
+    T('ComissaoDe10Porcento',
         amount=Percentual(10),
         to_=conta_comissao)
 
-    T('ImpostoPercentual',
+    T('ImpostoDe10Porcento',
         amount=Percentual(10),
         to_=conta_imposto)
 
     # defining the contract combining the transactions:
     VendaComImpostoEComissao =\
     T('VendaComImpostoEComissao')(
-        T('ComissaoPercentual')(
-            T('ImpostoPercentual')
+        T('ComissaoDe10Porcento')(
+            T('ImpostoDe10Porcento')
         ),
-        T('ImpostoPercentual')
+        T('ImpostoDe10Porcento')
     )
 
     # using the contract:
@@ -89,3 +89,98 @@ def test_venda_com_imposto_com_comissao_com_imposto_com_composicao_de_transacoes
     assert cash_out.balance() == Decimal(80)
     assert conta_comissao.balance() == Decimal(9)
     assert conta_imposto.balance() == Decimal(11)
+
+
+def test_venda_com_imposto_com_comissao_com_imposto_com_composicao_de_transacoes_2():
+    # accounts:
+    cash_in = conta()
+    cash_out = conta()
+    conta_comissao = conta()
+    conta_imposto = conta()
+
+    # defining the transactions:
+    T('VendaComImpostoEComissao',
+      from_=cash_in,
+      to_=cash_out)
+
+    T('ComissaoDe10Porcento',
+      amount=Percentual(10),
+      to_=conta_comissao)
+
+    T('ImpostoDe10Porcento',
+      amount=Percentual(10),
+      to_=conta_imposto)
+
+    # defining the contract combining the transactions different order:
+    VendaComImpostoEComissao2 =\
+    T('VendaComImpostoEComissao')(
+        T('ImpostoDe10Porcento'),
+        T('ComissaoDe10Porcento')(
+            T('ImpostoDe10Porcento')
+        )
+    )
+
+    # using the contract:
+    VendaComImpostoEComissao2(100).save()
+
+    assert Contract.objects.all().count() == 3
+
+    transactions = _Transaction.objects.all()
+    assert transactions.count() == 4
+    assert transactions[0].amount == Decimal(100)
+    assert transactions[1].amount == Decimal(10)
+    assert transactions[2].amount == Decimal(10)
+    assert transactions[3].amount == Decimal(1)
+
+    assert cash_in.balance() == Decimal(-100)
+    assert cash_out.balance() == Decimal(80)
+    assert conta_comissao.balance() == Decimal(9)
+    assert conta_imposto.balance() == Decimal(11)
+
+
+def test_venda_com_imposto_com_comissao_com_imposto_com_composicao_de_transacoes_3():
+    # accounts:
+    cash_in = conta()
+    cash_out = conta()
+    conta_comissao = conta()
+    conta_imposto = conta()
+
+    # defining the transactions:
+    T('VendaComImpostoEComissao',
+      from_=cash_in,
+      to_=cash_out)
+
+    T('ComissaoDe10Porcento',
+      amount=Percentual(10),
+      to_=conta_comissao)
+
+    T('ImpostoDe10Porcento',
+      amount=Percentual(10),
+      to_=conta_imposto)
+
+    # defining the contract combining the transactions different order:
+    VendaComImpostoEComissao2 =\
+    T('VendaComImpostoEComissao')(
+        T('ImpostoDe10Porcento')(
+            T('ComissaoDe10Porcento')(
+                T('ImpostoDe10Porcento')
+            )
+        )
+    )
+
+    # using the contract:
+    VendaComImpostoEComissao2(100).save()
+
+    assert Contract.objects.all().count() == 3
+
+    transactions = _Transaction.objects.all()
+    assert transactions.count() == 4
+    assert transactions[0].amount == Decimal(100)
+    assert transactions[1].amount == Decimal(10)
+    assert transactions[2].amount == Decimal(1)
+    assert transactions[3].amount == Decimal('0.10')
+
+    assert cash_in.balance() == Decimal(-100)
+    assert cash_out.balance() == Decimal(90)
+    assert conta_comissao.balance() == Decimal('0.90')
+    assert conta_imposto.balance() == Decimal('9.10')

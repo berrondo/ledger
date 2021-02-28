df36685abc6ffcd9e3bfe653c0ee25a27842560d
berrondo <claudio.berrondo@gmail.com>
2021-02-28 16:31:58 -0300

    adding admin site

--- ledger/core/admin.py
+++ ledger/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import (
+    Account,
+    Agreement,
+    Ledger,
+)
 
-# Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Agreement)
+admin.site.register(Ledger)

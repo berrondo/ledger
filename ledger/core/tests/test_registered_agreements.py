597e9a247f1fd01ca6bc2bb119e6b5865c4cfec4
berrondo <claudio.berrondo@gmail.com>
2021-03-01 01:29:54 -0300

    proposed test

--- ledger/core/tests/test_registered_agreements.py
+++ ledger/core/tests/test_registered_agreements.py
@@ -9,17 +9,23 @@ from ..register import AgreementRegister as R
 def test():
     # creating a Transaction by naming it automatically
     # registers its Agreement blue print to be reused:
-    t = T('VendaComplicada',
-          amount=100)
+    T('VendaComplicada',
+        amount=100)
 
-    assert Agreement.objects.all().count() == 1
+    T('ImpostoInjusto',
+        amount=Percentual(10))
+
+    assert Agreement.objects.all().count() == 2
 
     assert R.VendaComplicada
     assert type(R.VendaComplicada) == Agreement
     assert R.VendaComplicada.amount == '100'
 
+    assert R.ImpostoInjusto
+    assert type(R.ImpostoInjusto) == Agreement
+    assert R.ImpostoInjusto.amount == 'Percentual(10)'
+
     #using:
-    tt = T(R.VendaComplicada)(
-        T('ImpostoInjusto',
-          amount=Percentual(10))
-    )
+    # R.VendaComplicada()(
+    #     R.ImpostoInjusto()
+    # )
